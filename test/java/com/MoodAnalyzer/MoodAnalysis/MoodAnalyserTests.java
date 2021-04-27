295b84ff66ecabca882d92b85bacde15dae63671
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-27 09:26:10 +0530

    Refactor code: Given Empty Mood message should return invalid mood

--- test/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyserTests.java
+++ test/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyserTests.java
@@ -8,29 +8,19 @@ public class MoodAnalyserTests
 	@Test
     public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_SAD() throws CustomException
     {	
-		try {
-			String mood=MoodAnalyzer.MoodAnalyzer(" ");
-			
+			String mood=MoodAnalyzer.MoodAnalyzer(" ");	
 			mood=MoodAnalyzer.MoodAnalyzer();
-			
 	        assertEquals("Empty",mood);
-		}catch(CustomException e) {
-			System.out.println(e);
-		}
     }
 	
 	@Test
     public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_HAPPY () throws CustomException
     {	
-		try {
+		
 			String mood=MoodAnalyzer.MoodAnalyzer(" ");
-			
 			mood=MoodAnalyzer.MoodAnalyzer();
-			
 	        assertEquals("Empty",mood);
-		}catch(CustomException e) {
-			System.out.println(e);
-		}
+		
     }
     
 }
