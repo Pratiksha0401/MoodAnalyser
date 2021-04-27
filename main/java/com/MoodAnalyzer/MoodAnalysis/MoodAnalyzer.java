5d2cda3f27bcbb060071f8828bd14e27229939cb
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-27 08:25:42 +0530

    Added code: Given Null Mood message should return invalid mood

--- main/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyzer.java
+++ main/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyzer.java
@@ -4,29 +4,35 @@ import java.util.*;
 
 public class MoodAnalyzer 
 {	
-	public static String MoodAnalyzer() {
-		String message=null;
+	public static String MoodAnalyzer() throws CustomException {
+		Scanner sc=new Scanner(System.in);
+		String message=sc.nextLine();
 		try{
 			if(message.contains("Sad"))
 				return "SAD";
 			else
 				return "HAPPY";
 		}catch(NullPointerException e){
-			return "HAPPY";
+			throw new CustomException("Invalid Mood.....Enter valid Mood");
 		}
 	}
 	
-	public static String MoodAnalyzer(String message) {
+	public static String MoodAnalyzer(String message) throws CustomException {
 		try{
 			if(message.contains("Sad"))
 				return "SAD";
 			else
 				return "HAPPY";
 		}catch(NullPointerException e){
-			return "HAPPY";
+			throw new CustomException("Invalid Mood.....Enter valid mood ");
 		}
 	}
+}	
 
+	class CustomException extends Exception{
+		public CustomException(String message) {
+		   super(message);
+		}
+	}
 	
-	
-}
+
--- test/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyserTests.java
+++ test/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyserTests.java
@@ -8,11 +8,29 @@ public class MoodAnalyserTests
 	@Test
     public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_SAD()
     {	
-		String mood=MoodAnalyzer.MoodAnalyzer(null);
-		System.out.println(mood);
-		mood=MoodAnalyzer.MoodAnalyzer();
-		System.out.println(mood);
-        assertEquals("HAPPY",mood);
+		try {
+			String mood=MoodAnalyzer.MoodAnalyzer(null);
+			System.out.println(mood);
+			mood=MoodAnalyzer.MoodAnalyzer();
+			System.out.println(mood);
+	        assertEquals("SAD",mood);
+		}catch(CustomException e) {
+			System.out.println(e);
+		}
+    }
+	
+	@Test
+    public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_HAPPY()
+    {	
+		try {
+			String mood=MoodAnalyzer.MoodAnalyzer(null);
+			System.out.println(mood);
+			mood=MoodAnalyzer.MoodAnalyzer();
+			System.out.println(mood);
+	        assertEquals("HAPPY",mood);
+		}catch(CustomException e) {
+			System.out.println(e);
+		}
     }
     
 }
