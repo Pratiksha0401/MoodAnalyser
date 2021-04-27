07941909394a2ee934276a04290755a18715d59f
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-27 07:46:43 +0530

    Refactor code : Given Null Mood message should return HAPPY

--- test/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyserTests.java
+++ test/java/com/MoodAnalyzer/MoodAnalysis/MoodAnalyserTests.java
@@ -1,8 +1,6 @@
 package com.MoodAnalyzer.MoodAnalysis;
 
 import static org.junit.Assert.*;
-
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class MoodAnalyserTests 
@@ -10,7 +8,7 @@ public class MoodAnalyserTests
 	@Test
     public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_SAD()
     {	
-		String mood=MoodAnalyzer.MoodAnalyzer("I am in Happy mood");
+		String mood=MoodAnalyzer.MoodAnalyzer(null);
 		System.out.println(mood);
 		mood=MoodAnalyzer.MoodAnalyzer();
 		System.out.println(mood);
